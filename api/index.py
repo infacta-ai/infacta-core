0764bfdf8ec7a02cfee1cee2b36c1c4fb21ac05f
infacta-ai <infactaproject@gmail.com>
2026-03-13 12:24:25 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "text/plain"},
-        "body": "Infacta Core is running"
-    }
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Infacta Core is running")
