a8247f400337b2c07ce616d4c7d021beedb25156
infacta-ai <infactaproject@gmail.com>
2026-03-13 12:58:10 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,6 +3,6 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/plain")
+        self.send_header('Content-type','text/html')
         self.end_headers()
-        self.wfile.write(b"Infacta Core is running")
+        self.wfile.write(b"Infacta is starting...")
