08ae5f55be08b8ec287fc0662405aca5dc8f55ab
infacta-ai <infactaproject@gmail.com>
2026-03-17 00:07:01 +0200

    Fix formatting and add generate_questions to ACTIVE_MODULES

--- core/modules/module_registry.py
+++ core/modules/module_registry.py
@@ -3,10 +3,9 @@ from core.modules.structure_scan import scan_structure
 from core.modules.risk_scan import scan_risks
 from core.modules.questions_generator import generate_questions
 
-
 ACTIVE_MODULES = [
     preprocess_text,
     scan_structure,
     scan_risks,
-    generate_questions
+    generate_questions,
 ]
