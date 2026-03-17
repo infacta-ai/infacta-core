cebc65700db6678833ce8768292f9d239f5b3c70
infacta-ai <infactaproject@gmail.com>
2026-03-17 13:55:58 +0200

    Delete core directory

--- core/modules/preprocessing.py
+++
@@ -1,7 +0,0 @@
-from core.utils.text_cleaner import clean_text
-
-
-def preprocess_text(state: dict) -> dict:
-    raw_text = state.get("text", "")
-    state["processed_text"] = clean_text(raw_text)
-    return state
